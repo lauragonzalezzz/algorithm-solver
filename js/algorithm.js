169ead1b95c7b4b72de77d242526fdc7c4e13281
Laura <lg3295@gmail.com>
2016-03-25 10:18:43 -1000

    algorithm solver returns sum of square roots until argument number

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,3 +1,21 @@
-function algorithmSolver() {
-  return 5;
+function algorithmSolver(number) {
+  var squareRootsArr = [];
+  var sumOfSquareRoots = 0;
+
+  for (var i = 1; i < number; i++) {
+    var squareNum = Math.sqrt(i);
+    squareRootsArr.push(squareNum);
+  };
+
+  for (var k = 0; k < squareRootsArr.length; k++) {
+    sumOfSquareRoots += squareRootsArr[k];
+  };
+
+  sumOfSquareRoots = sumOfSquareRoots.toFixed(2);
+  sumOfSquareRoots = Number(sumOfSquareRoots);
+  console.log('sumOfSquareRoots',sumOfSquareRoots);
+
+  return sumOfSquareRoots;
 };
+
+
