4e990ed7e809a38ff43831a611660491c4c07a61
Laura <lg3295@gmail.com>
2016-03-25 10:19:10 -1000

    tests to ensure algorithm solver returns the sum of square roots

--- js/test/algorithm.spec.js
+++ js/test/algorithm.spec.js
@@ -1,11 +1,22 @@
 describe ('algorithmSolver', function (){
 
+
   it('should be a function', function() {
     expect(algorithmSolver).to.be.a('function');
   });
 
   it('should return a number', function() {
     expect(algorithmSolver()).to.be.a('number');
+  });
+
+  it('should return the sum of square roots', function() {
+    var result = algorithmSolver(4);
+    expect(result).to.equal(4.15);
+    var result2 = algorithmSolver(5);
+    expect(result).to.equal(6.15);
+    var result3 = algorithmSolver(19);
+    expect(result).to.equal(52.83);
+
   });
 
 
